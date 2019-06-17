0b526533605a18f8d97bb5675b3ecc4b814aac53
Gnusson.net <40320838+GnussonNet@users.noreply.github.com>
2019-06-17 10:49:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ echo "
                                                 ";
 
 echo "[✔] Checking directories...";
-if [ -d "/usr/share/doc/Menu" ] ;
+if [ -d "/usr/share/doc/MyMenu" ] ;
 then
 echo "[◉] A directory Menu was found! Do you want to replace it? [Y/n]:" ; 
 read mama
 if [ $mama == "y" ] ; 
 then
- rm -R "/usr/share/doc/Menu"
+ rm -R "/usr/share/doc/MyMenu"
 else
  exit
 fi
@@ -27,21 +27,21 @@ fi
 
  echo "[✔] Installing ...";
  echo "";
- git clone https://github.com/GnussonNet/Menu.git;
+ git clone https://github.com/GnussonNet/MyMenu.git;
  echo "#!/bin/bash 
- python /usr/share/doc/Menu/Menu.py" '${1+"$@"}' > Menu;
- chmod +x Menu;
- sudo cp Menu /usr/bin/;
- rm Menu;
+ python /usr/share/doc/MyMenu/Menu.py" '${1+"$@"}' > menu;
+ chmod +x menu;
+ sudo cp menu /usr/bin/;
+ rm menu;
 
 
-if [ -d "/usr/share/doc/Menu" ] ;
+if [ -d "/usr/share/doc/MyMenu" ] ;
 then
 echo "";
 echo "[✔]Tool istalled with success![✔]";
 echo "";
   echo "[✔]====================================================================[✔]";
-  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing Menu  !     ✔✔✔ [✔]"; 
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing menu!        ✔✔✔ [✔]"; 
   echo "[✔]====================================================================[✔]";
   echo "";
 else
