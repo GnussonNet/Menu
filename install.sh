fab0f0eb51a8114d8d8118d46806146aff1ae1b1
Gnusson.net <40320838+GnussonNet@users.noreply.github.com>
2019-06-17 10:28:55 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,12 +7,27 @@ echo "
 ██║╚██╔╝██║██╔══██║██║╚██╗██║██║╚════██║╚════██║██║   ██║
 ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║███████║███████║╚██████╔╝
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝ 
-▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Gnusse
+▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Ⓜ Ⓐ Ⓝ Ⓘ Ⓢ Ⓢ Ⓞ  ☪ ~
   █   █  █ █  █ █   ▀▀█ 
   ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀             
+                                                ";
+
+echo "[✔] Checking directories...";
+if [ -d "/usr/share/doc/Menu" ] ;
+then
+echo "[◉] A directory Menu was found! Do you want to replace it? [Y/n]:" ; 
+read mama
+if [ $mama == "y" ] ; 
+then
+ rm -R "/usr/share/doc/Menu"
+else
+ exit
+fi
+fi
 
  echo "[✔] Installing ...";
  echo "";
+ git clone https://github.com/GnussonNet/menu.git;
  echo "#!/bin/bash 
  python /usr/share/doc/Menu/Menu.py" '${1+"$@"}' > menu;
  chmod +x menu;
@@ -26,7 +41,7 @@ echo "";
 echo "[✔]Tool istalled with success![✔]";
 echo "";
   echo "[✔]====================================================================[✔]";
-  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing crips  !     ✔✔✔ [✔]"; 
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing menu  !     ✔✔✔ [✔]"; 
   echo "[✔]====================================================================[✔]";
   echo "";
 else
