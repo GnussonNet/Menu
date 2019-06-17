68e1bb71b67f4ee753d463b452a782f84bf2d130
Gnusson.net <40320838+GnussonNet@users.noreply.github.com>
2019-06-17 10:36:05 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -27,12 +27,12 @@ fi
 
  echo "[✔] Installing ...";
  echo "";
- git clone https://github.com/GnussonNet/menu.git;
+ git clone https://github.com/GnussonNet/Menu.git;
  echo "#!/bin/bash 
- python /usr/share/doc/Menu/Menu.py" '${1+"$@"}' > menu;
- chmod +x menu;
- sudo cp menu /usr/bin/;
- rm menu;
+ python /usr/share/doc/Menu/Menu.py" '${1+"$@"}' > Menu;
+ chmod +x Menu;
+ sudo cp Menu /usr/bin/;
+ rm Menu;
 
 
 if [ -d "/usr/share/doc/Menu" ] ;
@@ -41,7 +41,7 @@ echo "";
 echo "[✔]Tool istalled with success![✔]";
 echo "";
   echo "[✔]====================================================================[✔]";
-  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing menu  !     ✔✔✔ [✔]"; 
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing Menu  !     ✔✔✔ [✔]"; 
   echo "[✔]====================================================================[✔]";
   echo "";
 else
