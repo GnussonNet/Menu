efc67e667161840a6b7d37be28936459c8b1b716
Gnusson.net <40320838+GnussonNet@users.noreply.github.com>
2019-06-17 10:22:25 +0200

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+os.system("clear && clear && clear")
+
+menu = '''\033[0m
+    {1}--Home
+    {2}--Desktop
+    {3}--Downloads
+    {4}--Crips
+    {5}--HiddenEye 
+    {6}--Comming soon
+    {7}--Comming soon
+    {0}--Comming soon
+    {99}-Exit                                                                                                                   
+ '''
+
+print menu
+
+def quit():
+            con = raw_input('Continue [Y/n] -> ')
+            if con[0].upper() == 'N':
+		print(os.getcwd())
+		os.chdir("/root")
+		print(os.getcwd())
+                
+            else:
+                os.system("clear")
+                print menu
+                select()
+           
+
+def  select():
+  try:
+    choice = input("Menu~# ")
+    if choice == 1:
+	print("1")
+	quit()
+
+    elif choice == 2:
+	print("2")
+	quit()
+
+    elif choice == 3:
+	print("3")
+	quit()
+ 
+    elif choice == 0:
+	print("0")
+	quit()
+
+    elif choice == 4:
+	print("4")
+	quit()
+
+    elif choice == 5:
+	print("5")
+	quit()
+
+    elif choice == 6:
+	print("6")
+	quit()
+
+    elif choice == 7:
+	print("7")
+	quit()
+
+  except(KeyboardInterrupt):
+    print ""
+select()
